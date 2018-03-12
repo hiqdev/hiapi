e6b9370f71a4d0bee67a7dcf20ca8ebbdf5f9c52
Andrii Vasyliev <sol@hiqdev.com>
2018-03-12 19:44:29 +0200

    added `ApiCommandsBusInterface`

---
+++ src/bus/ApiCommandsBusInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * HiAPI Yii2 base project for building API
+ *
+ * @link      https://github.com/hiqdev/hiapi
+ * @package   hiapi
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiapi\bus;
+
+use hiqdev\yii2\autobus\components\AutoBusInterface;
+
+interface ApiCommandsBusInterface extends AutoBusInterface
+{
+}
