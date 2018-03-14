97924336bd5273a251f1c909f69d68d2d25ecdb1
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-03-14 20:17:41 +0200

    Added ValidationException

---
+++ src/exceptions/domain/ValidationException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace hiapi\exceptions\domain;
+
+/**
+ * Class ValidationException
+ *
+ * @author Dmytro Naumenko <d.naumenko.a@gmail.com>
+ */
+class ValidationException extends DomainException
+{
+
+}
