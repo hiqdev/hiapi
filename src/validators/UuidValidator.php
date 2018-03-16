152b3e06ede10d166b99ecc08c926a8bf6c7122b
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-03-16 16:55:13 +0200

    Fixed regex

--- src/validators/UuidValidator.php
+++ src/validators/UuidValidator.php
@@ -11,14 +11,7 @@ use yii\validators\RegularExpressionValidator;
  */
 class UuidValidator extends RegularExpressionValidator
 {
-    public $pattern = '~^[0-9a-f]{8}-[0-9a-f]{4}-1[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$~i';
-
-    /**
-     * The version of the UUID you'd like to check.
-     *
-     * @var int
-     */
-    public $version;
+    public $pattern = '/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i';
 
     public function __construct($config = [])
     {
