19e35222a5f49b2c1dfc3bc3d06e9b524ace8538
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-04-03 16:41:07 +0300

    Enhanced ConfigurableEmitter to support event masks

--- src/event/ConfigurableEmitter.php
+++ src/event/ConfigurableEmitter.php
@@ -4,8 +4,11 @@ namespace hiapi\event;
 
 use League\Event\Emitter;
 use League\Event\EmitterInterface;
+use League\Event\ListenerInterface;
 use yii\base\InvalidConfigException;
 use yii\base\UnknownPropertyException;
+use yii\di\Container;
+use yii\helpers\StringHelper;
 
 /**
  * Class ConfigurableEmitter
@@ -14,6 +17,31 @@ use yii\base\UnknownPropertyException;
  */
 class ConfigurableEmitter extends Emitter implements EmitterInterface
 {
+    /**
+     * @var Container
+     */
+    private $container;
+
+    /**
+     * ConfigurableEmitter constructor.
+     *
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function __set($name, $value)
+    {
+        if ($name === 'listeners') {
+            $this->setListeners($value);
+            return;
+        }
+
+        throw new UnknownPropertyException('Property "' . $name . '" is not available.');
+    }
+
     /**
      * Allows to set listeners with standard Yii configuration system
      *
@@ -29,10 +57,7 @@ class ConfigurableEmitter extends Emitter implements EmitterInterface
             }
 
             if (is_string($listener['listener'])) {
-                $className = $listener['listener'];
-                $listener['listener'] = function ($event) use ($className) {
-                    (new $className())->handle($event);
-                };
+                $listener['listener'] = $this->listenerFromClassName($listener['listener']);
             }
 
             $this->addListener($listener['event'], $listener['listener'], $listener['priority'] ?? self::P_NORMAL);
@@ -41,13 +66,66 @@ class ConfigurableEmitter extends Emitter implements EmitterInterface
         return $this;
     }
 
-    public function __set($name, $value)
+    /**
+     * @param $className
+     * @return \Closure
+     */
+    private function listenerFromClassName(string $className): \Closure
     {
-        if ($name === 'listeners') {
-            $this->setListeners($value);
-            return;
+        return function ($event) use ($className) {
+            /** @var ListenerInterface $handler */
+            $listener = $this->container->get($className);
+
+            $listener->handle($event);
+        };
+    }
+
+    public function hasListeners($event)
+    {
+        if (parent::hasListeners($event)) {
+            return true;
         }
 
-        throw new UnknownPropertyException('Property "' . $name . '" is not available.');
+        $namesWithWildcard = array_filter(array_keys($this->listeners), function ($name) {
+            return $name !== '*' && strpos($name, '*') !== false;
+        });
+        foreach ($namesWithWildcard as $name) {
+            if (StringHelper::matchWildcard('*', $name)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the listeners sorted by priority for a given event.
+     *
+     * @param string $event
+     *
+     * @return ListenerInterface[]
+     */
+    protected function getSortedListeners($event)
+    {
+        if (! $this->hasListeners($event)) {
+            return [];
+        }
+
+        $listeners = $this->listeners[$event] ?? [];
+
+        if ($event !== '*') {
+            $namesWithWildcard = array_filter(array_keys($this->listeners), function ($name) {
+                return $name !== '*' && strpos($name, '*') !== false;
+            });
+            foreach ($namesWithWildcard as $name) {
+                if (StringHelper::matchWildcard('*', $name)) {
+                    $listeners = array_merge($listeners, $this->listeners[$name]);
+                }
+            }
+        }
+
+        krsort($listeners);
+
+        return call_user_func_array('array_merge', $listeners);
     }
 }
