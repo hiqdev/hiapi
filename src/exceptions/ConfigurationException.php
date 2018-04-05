f4979a25b98872bad378381c58edaf770fba4711
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-04-05 20:25:11 +0300

    Added ConfigurationException

---
+++ src/exceptions/ConfigurationException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hiapi\exceptions;
+
+/**
+ * Class ConfigurationException
+ *
+ * @author Dmytro Naumenko <d.naumenko.a@gmail.com>
+ */
+class ConfigurationException extends \Exception
+{
+}
