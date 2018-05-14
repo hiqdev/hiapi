4016107e0f2853928382b0764be2302733708db3
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-05-14 16:58:58 +0300

    Allowed JsonApi content-type in ApiController

--- src/controllers/ApiController.php
+++ src/controllers/ApiController.php
@@ -56,6 +56,7 @@ class ApiController extends Controller
                 'class' => ContentNegotiator::class,
                 'formats' => [
                     'application/json' => Response::FORMAT_JSON,
+                    'application/vnd.api+json' => Response::FORMAT_JSON,
                 ],
             ],
             'auth' => [
