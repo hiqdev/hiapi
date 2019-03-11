2a47fbd8ec9502a40e0d8c12f9e05410b5817320
Andrii Vasyliev <sol@hiqdev.com>
2019-03-11 09:12:48 +0000

    Making Yii 2/3 compatible config

--- config/common.php
+++ config/common.php
@@ -8,16 +8,22 @@
  * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
  */
 
-use yii\di\Reference;
+use hiapi\yii;
 
-return [
-    'app' => [
-        'id' => 'hiapi',
-        'name' => 'HiAPI',
-        'basePath' => dirname(__DIR__) . '/src',
-        'viewPath' => '@hiapi/views',
+$app = [
+    'id' => 'hiapi',
+    'name' => 'HiAPI',
+    'basePath' => dirname(__DIR__) . '/src',
+    'viewPath' => '@hiapi/views',
+];
+
+$components = [
+    'user' => [
+        '__class' => \yii\web\User::class,
     ],
+];
 
+$singletons = [
     \yii\web\User::class => [
         'identityClass' => \hiapi\models\HiamUserIdentity::class,
         'enableSession' => false,
@@ -26,7 +32,7 @@ return [
     \hiapi\bus\ApiCommandsBusInterface::class => [
         '__class' => \hiapi\bus\ApiCommandsBus::class,
         '__construct()' => [
-            'bus' => Reference::to('bus.the-bus'),
+            yii::referenceTo('bus.the-bus'),
         ],
     ],
     'bus.per-command-middleware' => [
@@ -35,15 +41,15 @@ return [
     'bus.default-command-handler' => [
         '__class' => \League\Tactician\Handler\CommandHandlerMiddleware::class,
         '__construct()' => [
-            Reference::to(\League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor::class),
-            Reference::to(\hiqdev\yii2\autobus\bus\NearbyHandlerLocator::class),
-            Reference::to(\League\Tactician\Handler\MethodNameInflector\HandleInflector::class),
+            yii::referenceTo(\League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor::class),
+            yii::referenceTo(\hiqdev\yii2\autobus\bus\NearbyHandlerLocator::class),
+            yii::referenceTo(\League\Tactician\Handler\MethodNameInflector\HandleInflector::class),
         ],
     ],
     'bus.the-bus' => [
         '__class' => \hiqdev\yii2\autobus\components\TacticianCommandBus::class,
         '__construct()' => [
-            Reference::to('bus.default-command-handler'),
+            yii::referenceTo('bus.default-command-handler'),
         ],
         'middlewares' => [
             'bus.responder-middleware',
@@ -88,3 +94,54 @@ return [
         return Yii::$app->get('mailer');
     },
 ];
+
+$old_singletons = [
+    \yii\base\Application::class => function () {
+        return Yii::$app;
+    },
+
+    \Psr\Log\LoggerInterface::class => function () {
+        return Yii::getLogger();
+    },
+
+    \Psr\Container\ContainerInterface::class => function ($container) {
+        return new class($container) implements \Psr\Container\ContainerInterface {
+            /**
+             * @var \yii\di\Container
+             */
+            private $yiiContainer;
+
+            public function __construct(\yii\di\Container $yiiContainer)
+            {
+                $this->yiiContainer = $yiiContainer;
+            }
+
+            public function get($id)
+            {
+                return $this->yiiContainer->get($id);
+            }
+
+            public function has($id)
+            {
+                return $this->yiiContainer->has($id);
+            }
+        };
+    },
+];
+
+return class_exists('Yii') ? array_merge([
+    'aliases' => $aliases,
+    'logger' => [
+        '__class' => \yii\log\Logger::class
+    ],
+    'components' => $components,
+    'container' => [
+        'singletons' => array_merge($singletons, $old_singletons),
+    ],
+    'params' => $params,
+    'vendorPath' => '@root/vendor',
+    'runtimePath' => '@root/runtime',
+], $app) : array_merge([
+    'aliases' => $aliases,
+    'app' => $app,
+], $components, $singletons);
