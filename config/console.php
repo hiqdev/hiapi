aaf68700a4cce8bb1930478b5cebbcb21944dfe9
Andrii Vasyliev <sol@hiqdev.com>
2019-11-06 16:19:01 +0000

    Fixing Yii 2/3 compatibility more again

--- config/console.php
+++ config/console.php
@@ -41,46 +41,47 @@ return \hiqdev\yii\compat\yii::is2() ? array_merge([
     'container' => [
         'singletons' => $singletons,
     ],
-    'logger' => [
-        '__class' => \yii\log\Logger::class,
-        'flushInterval' => 1,
-        'targets' => [
-            [
-                '__class' => \hiapi\console\ConsoleLogTarget::class,
-                'exportContext' => YII_ENV === 'dev' ? [
-                    Psr\Log\LogLevel::EMERGENCY => true,
-                    Psr\Log\LogLevel::ERROR     => true,
-                    Psr\Log\LogLevel::ALERT     => true,
-                    Psr\Log\LogLevel::CRITICAL  => true,
-                    Psr\Log\LogLevel::WARNING   => true,
-                    Psr\Log\LogLevel::NOTICE    => true,
-                    Psr\Log\LogLevel::INFO      => true,
-                    Psr\Log\LogLevel::DEBUG     => true,
-                ] : [
-                    Psr\Log\LogLevel::EMERGENCY => false,
-                    Psr\Log\LogLevel::ERROR     => false,
-                    Psr\Log\LogLevel::ALERT     => false,
-                    Psr\Log\LogLevel::CRITICAL  => false,
-                    Psr\Log\LogLevel::WARNING   => false,
+    'components' => [
+        'log' => [
+            'flushInterval' => 1,
+            'targets' => [
+                [
+                    '__class' => \hiapi\console\ConsoleLogTarget::class,
+                    'exportContext' => YII_ENV === 'dev' ? [
+                        Psr\Log\LogLevel::EMERGENCY => true,
+                        Psr\Log\LogLevel::ERROR     => true,
+                        Psr\Log\LogLevel::ALERT     => true,
+                        Psr\Log\LogLevel::CRITICAL  => true,
+                        Psr\Log\LogLevel::WARNING   => true,
+                        Psr\Log\LogLevel::NOTICE    => true,
+                        Psr\Log\LogLevel::INFO      => true,
+                        Psr\Log\LogLevel::DEBUG     => true,
+                    ] : [
+                        Psr\Log\LogLevel::EMERGENCY => false,
+                        Psr\Log\LogLevel::ERROR     => false,
+                        Psr\Log\LogLevel::ALERT     => false,
+                        Psr\Log\LogLevel::CRITICAL  => false,
+                        Psr\Log\LogLevel::WARNING   => false,
+                    ],
+                    'styles' => YII_ENV === 'dev' ? [
+                        Psr\Log\LogLevel::EMERGENCY => [yii\helpers\Console::BOLD, yii\helpers\Console::BG_RED],
+                        Psr\Log\LogLevel::ERROR     => [yii\helpers\Console::FG_RED, yii\helpers\Console::BOLD],
+                        Psr\Log\LogLevel::ALERT     => [yii\helpers\Console::FG_RED],
+                        Psr\Log\LogLevel::CRITICAL  => [yii\helpers\Console::FG_RED],
+                        Psr\Log\LogLevel::WARNING   => [yii\helpers\Console::FG_YELLOW],
+                        Psr\Log\LogLevel::NOTICE    => [],
+                        Psr\Log\LogLevel::INFO      => [],
+                        Psr\Log\LogLevel::DEBUG     => [yii\helpers\Console::FG_GREEN],
+                    ] : [
+                        Psr\Log\LogLevel::EMERGENCY => [yii\helpers\Console::BOLD, yii\helpers\Console::BG_RED],
+                        Psr\Log\LogLevel::ERROR     => [yii\helpers\Console::FG_RED, yii\helpers\Console::BOLD],
+                        Psr\Log\LogLevel::ALERT     => [yii\helpers\Console::FG_RED],
+                        Psr\Log\LogLevel::CRITICAL  => [yii\helpers\Console::FG_RED],
+                        Psr\Log\LogLevel::WARNING   => [yii\helpers\Console::FG_YELLOW],
+                    ],
                 ],
-                'styles' => YII_ENV === 'dev' ? [
-                    Psr\Log\LogLevel::EMERGENCY => [yii\helpers\Console::BOLD, yii\helpers\Console::BG_RED],
-                    Psr\Log\LogLevel::ERROR     => [yii\helpers\Console::FG_RED, yii\helpers\Console::BOLD],
-                    Psr\Log\LogLevel::ALERT     => [yii\helpers\Console::FG_RED],
-                    Psr\Log\LogLevel::CRITICAL  => [yii\helpers\Console::FG_RED],
-                    Psr\Log\LogLevel::WARNING   => [yii\helpers\Console::FG_YELLOW],
-                    Psr\Log\LogLevel::NOTICE    => [],
-                    Psr\Log\LogLevel::INFO      => [],
-                    Psr\Log\LogLevel::DEBUG     => [yii\helpers\Console::FG_GREEN],
-                ] : [
-                    Psr\Log\LogLevel::EMERGENCY => [yii\helpers\Console::BOLD, yii\helpers\Console::BG_RED],
-                    Psr\Log\LogLevel::ERROR     => [yii\helpers\Console::FG_RED, yii\helpers\Console::BOLD],
-                    Psr\Log\LogLevel::ALERT     => [yii\helpers\Console::FG_RED],
-                    Psr\Log\LogLevel::CRITICAL  => [yii\helpers\Console::FG_RED],
-                    Psr\Log\LogLevel::WARNING   => [yii\helpers\Console::FG_YELLOW],
-                ],
-            ],
-        ]
+            ]
+        ],
     ],
 ], $app) : array_merge([
     'app' => $app,
