f2ffe5e17dca4f6db1a3cadf4e87860ccee9396b
Andrii Vasyliev <sol@hiqdev.com>
2020-03-16 17:35:26 +0000

    Fixed `CIDR` class to work with range without network length, added miniamal tests

--- src/Utils/CIDR.php
+++ src/Utils/CIDR.php
@@ -4,9 +4,13 @@ namespace hiapi\Core\Utils;
 
 class CIDR
 {
-    public static function match ($ip, $range)
+    public static function match($ip, $range): bool
     {
+        if (strpos($range, '/') === false) {
+            $range = "$range/32";
+        }
         list ($subnet, $bits) = explode('/', $range);
+
         $ip = ip2long($ip);
         $subnet = ip2long($subnet);
         $mask = -1 << (32 - $bits);
@@ -14,7 +18,7 @@ class CIDR
         return ($ip & $mask) == $subnet;
     }
 
-    public static function matchBulk ($ip, $ranges)
+    public static function matchBulk($ip, $ranges): bool
     {
         $match = false;
         foreach ($ranges as $range => $value) {
---
+++ tests/unit/Utils/CIDRTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace hiapi\tests\unit\Utils;
+
+use hiapi\Core\Utils\CIDR;
+
+class CIDRTest extends \PHPUnit\Framework\TestCase
+{
+    protected $builder;
+
+    public function testMatch()
+    {
+        $this->assertTrue(CIDR::match('1.1.1.1',        '1.1.1.1/32'));
+        $this->assertTrue(CIDR::match('1.1.1.1',        '1.1.1.0/24'));
+        $this->assertTrue(CIDR::match('1.1.1.255',      '1.1.1.0/24'));
+
+        $this->assertFalse(CIDR::match('1.1.1.1',       '1.1.1.0/32'));
+        $this->assertFalse(CIDR::match('1.1.1.1',       '2.2.2.0/24'));
+    }
+
+    public function testMatchBulk()
+    {
+        $this->assertTrue(CIDR::matchBulk('1.1.1.1',    ['0.0.0.0/0' => 1]));
+        $this->assertTrue(CIDR::matchBulk('1.1.1.1',    ['1.1.1.1/32' => 1]));
+        $this->assertTrue(CIDR::matchBulk('1.1.1.1',    ['2.2.2.2' => 1, '1.1.1.0/24' => 1]));
+        $this->assertTrue(CIDR::matchBulk('1.1.1.255',  ['1.1.1.0/24' => 1, '2.2.2.2' => 1]));
+
+        $this->assertFalse(CIDR::matchBulk('1.1.1.1',   ['2.2.2.2/32' => 1, '1.1.1.0/32' => 1]));
+        $this->assertFalse(CIDR::matchBulk('1.1.1.1',   ['3.3.3.3' =>1, '2.2.2.0/24' => 1]));
+    }
+}
