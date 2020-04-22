bf162061061bb0fabb7240e2656895ed73402e44
Andrii Vasyliev <sol@hiqdev.com>
2020-04-22 14:04:48 +0000

    Inited `UsernameValidator`

---
+++ src/validators/UsernameValidator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace hiapi\validators;
+
+use yii\validators\RegularExpressionValidator;
+
+/**
+ * Class UsernameValidator validates username
+ * TODO: allow emails also?
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class UsernameValidator extends RegularExpressionValidator
+{
+    public $pattern = '/^[a-z][a-z0-9_]{2,31}$/';
+
+    public function init()
+    {
+        parent::init();
+
+        $this->message = 'Value is not a valid username';
+    }
+}
