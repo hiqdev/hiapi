f0522aa1e7429fd3dfebd282ca622c1322296606
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2020-08-26 18:12:01 +0300

    Added HiapiException, NotAuthorizedException

---
+++ src/exceptions/HiapiException.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace hiapi\exceptions;
+
+/**
+ * Class HiapiException can be used for exceptions, that contains a user-safe message
+ *
+ * @author Dmytro Naumenko <d.naumenko.a@gmail.com>
+ */
+class HiapiException extends \RuntimeException
+{
+
+}
