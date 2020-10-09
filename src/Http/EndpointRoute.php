23dba20c1d18268eff6591dd6fd11857287ff16f
Andrii Vasyliev <sol@hiqdev.com>
2020-10-09 14:00:44 +0000

    Extend `EndpointRoute` with all methods and changeable middleware builder

--- src/Http/EndpointRoute.php
+++ src/Http/EndpointRoute.php
@@ -8,8 +8,48 @@ use Yiisoft\Router\Route;
 
 class EndpointRoute
 {
-    public static function get(string $pattern, string $command): Route
+    public static function get(string $pattern, string $name): Route
     {
-        return Route::get($pattern, EndpointMiddleware::for($command));
+        return Route::get($pattern, static::buildMiddlewareFor($name));
+    }
+
+    public static function post(string $pattern, string $name): Route
+    {
+        return Route::post($pattern, static::buildMiddlewareFor($name));
+    }
+
+    public static function put(string $pattern, string $name): Route
+    {
+        return Route::put($pattern, static::buildMiddlewareFor($name));
+    }
+
+    public static function delete(string $pattern, string $name): Route
+    {
+        return Route::delete($pattern, static::buildMiddlewareFor($name));
+    }
+
+    public static function patch(string $pattern, string $name): Route
+    {
+        return Route::patch($pattern, static::buildMiddlewareFor($name));
+    }
+
+    public static function head(string $pattern, string $name): Route
+    {
+        return Route::head($pattern, static::buildMiddlewareFor($name));
+    }
+
+    public static function options(string $pattern, string $name): Route
+    {
+        return Route::options($pattern, static::buildMiddlewareFor($name));
+    }
+
+    public static function anyMethod(string $pattern, string $name): Route
+    {
+        return Route::anyMethod($pattern, static::buildMiddlewareFor($name));
+    }
+
+    public static function buildMiddlewareFor(string $name): \Closure
+    {
+        return EndpointMiddleware::for($name);
     }
 }
