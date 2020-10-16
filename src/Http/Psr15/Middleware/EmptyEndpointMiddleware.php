5f222e585ab4fdf1b61721f7b85ba2cf9f7df0b9
Maksym Storchak <smy980807@ukr.net>
2020-10-16 16:01:49 +0300

    created empty endpoint to handle request without db connect (#12)
    * created empty endpoint which not using db connect to handle request
    
    * codestyle

---
+++ src/Http/Psr15/Middleware/EmptyEndpointMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace hiapi\Core\Http\Psr15\Middleware;
+
+use hiapi\Core\Http\Psr7\Response\FatResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class EmptyEndpointMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if ($request->getUri()->getPath() === '/emptyEndpoint') {
+            return FatResponse::create([], $request);
+        }
+
+        return $handler->handle($request);
+    }
+}
