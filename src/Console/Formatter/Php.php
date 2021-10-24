13449c55371af176fa57b3ae0d680698a3dfc44d
Andrey Klochok <andreyklochok@gmail.com>
2021-10-24 18:49:13 +0300

    Fix request to `index.php` in URL

--- src/Console/Formatter/Php.php
+++ src/Console/Formatter/Php.php
@@ -2,12 +2,17 @@
 
 namespace hiapi\Core\Console\Formatter;
 
+use Exception;
 use Lcobucci\ContentNegotiation\Formatter\ContentOnly;
 
 class Php extends ContentOnly
 {
     public function formatContent($content, $attributes = []): string
     {
+        if ($content instanceof Exception) {
+            throw $content;
+        }
+
         return var_export($content, true);
     }
 }
